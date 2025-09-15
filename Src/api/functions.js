c0edbb168ec70c72359f82fe1cdd3d44ad87bd12
DarrylBard13 <darryl.bard13@gmail.com>
2025-09-15 16:11:06 -0400

    Add files via upload

---
+++ Src/api/functions.js
@@ -0,0 +1,13 @@
+import { base44 } from './base44Client';
+
+
+export const getBills = base44.functions.getBills;
+
+export const runBillRollover = base44.functions.runBillRollover;
+
+export const catchUpBillAdvancements = base44.functions.catchUpBillAdvancements;
+
+export const rollover_manager = base44.functions.rollover_manager;
+
+export const updateAgent = base44.functions.updateAgent;
+
