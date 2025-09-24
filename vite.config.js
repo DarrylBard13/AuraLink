29b88bb8388f293d1444addc6ef6f926139c0e79
DarrylBard13 <darryl.bard13@gmail.com>
2025-09-24 19:57:49 +0000

    Refactor project structure and configuration files; add new .gitignore entries, restore index.html, postcss.config.js, tailwind.config.js, and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -96,7 +96,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('.', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
